7968d3db759f912c9bdd6f6b08734b467e8bde03
estebanpoly1 <estebanpoly.e.a@gmail.com>
2022-11-09 01:50:43 -0500

    update UUID

---
+++ src/mundoorigami_Crosscuting/com.origami.mundoorigami.crosscuting/UUID.js
@@ -0,0 +1,29 @@
+class UUID {
+
+
+    /*constructor (UUID, Estado) {
+      this.UUID = UUID;
+      this.Estado = Estado;
+
+    }*/
+
+
+    generarUuid() {
+      let ahora = new Date().getTime();
+      let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          let aleatorio = (ahora + Math. random() * 16) % 16 | 0;
+          ahora = Math.floor(ahora / 16);
+          return (c == 'x' ? aleatorio : (aleatorio & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+      
+      
+    }
+
+  
+  }
+
+const uuid = new  UUID();
+  
+module.exports = {uuid};
+
--- src/mundoorigami_Domain/com.origami.mundoorigami.domain/Persona.js
+++ src/mundoorigami_Domain/com.origami.mundoorigami.domain/Persona.js
@@ -1,8 +1,24 @@
+const {} = require("UUID");
+const { uuid } = require("../../mundoorigami_Crosscuting/com.origami.mundoorigami.crosscuting/UUID");
+
+
 class Persona {
+
+  UUID = uuid.generarUuid();
+  nombre = "";
+  correo = "";
+  contraseña ="";
+
+
+
     constructor (UUID, nombre, correo, contraseña) {
-      this.UUID = UUID;
+      this.UUID = uuid.generarUuid();
       this.nombre = nombre;
       this.correo = correo;
       this.contraseña = contraseña;
     }
   }
+
+
+  const idPerson = new Persona();
+  console.log(idPerson.UUID);
